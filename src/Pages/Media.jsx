f23f051a430a8b019f92d8c223df3b917a7d6c80
anie308 <aniebietabasiobo@gmail.com>
2022-04-14 15:58:39 +0100

    commit

--- src/Pages/Media.jsx
+++ src/Pages/Media.jsx
@@ -1,32 +1,31 @@
 import React from "react";
 import UploadCard from "../components/Upload/UploadCard/UploadCard";
-import {AiOutlinePlus} from 'react-icons/ai'
-import {BsImage} from 'react-icons/bs'
-import { ImageConfig } from "../components/ImageDefault";
+import { AiOutlinePlus, AiTwotoneFolderOpen } from "react-icons/ai";
+import { BsImage, BsImageFill } from "react-icons/bs";
 import {
   Container,
   UploadMCon,
   UploadText,
   FileUploadCon,
   Folder,
-  UploadArea
+  UploadArea,
 } from "../styles/Media.style";
 import { useState } from "react";
 
 function Media() {
   const onFileChange = (files) => {
-    console.log(files)
-  }
+    console.log(files);
+  };
 
-  const [file, setFile] = useState([])   
+  const [file, setFile] = useState([]);
   const onFileDrop = (e) => {
     const newFile = e.target.files[0];
-    if(newFile) {
+    if (newFile) {
       const updatedList = [...file, newFile];
-      setFile(updatedList)
-      onFileChange(updatedList)
+      setFile(updatedList);
+      onFileChange(updatedList);
     }
-  }
+  };
   return (
     <Container>
       <UploadMCon>
@@ -39,43 +38,46 @@ function Media() {
           <div className="menu-header">
             <div className="menu-title">Menu Folder</div>
             <div className="create">
-              <AiOutlinePlus/>
+              <AiOutlinePlus />
               <p>Create Folder</p>
-           
             </div>
           </div>
           <div>
-              <div>
-             {
-                file.length > 0 ? ( 
+            <div className="folder1">
+              <div className="identify">
+                <AiTwotoneFolderOpen className="fold-icon" />
+                <p>Folder 1</p>
+              </div>
+              {file.length > 0 ? (
+                <div>
                   <div className="drop-file">
-                    <p>Ready to upload</p>
-                    {
-                      file.map((item, index) => (
-                        <div key={index} className="file-item">
-                          <img src={ImageConfig[item.type.split('/')[1] || ImageConfig['default']]} alt="" />
-                          <p>{item.name} </p>
-                          <p>{item.size}</p>
-                        </div>
-                      ))
-                    }
-                  </div>
-                ): null
-              }
-             </div>
+                  {file.map((item, index) => (
+                    <div key={index} className="file-item">
+                      <BsImageFill className="med-icon" />
+                      <p className="item-name">{item.name} </p>
+                    </div>
+                  ))}
+                </div>
+                   <div className="add">
+                Add Image
+
+              </div>
+                </div>
+                
+                
+              ) : null}
+             
+            </div>
+           
           </div>
-
         </Folder>
-        <UploadArea 
-        onFileChange={(files) => onFileChange(files)}
-        >
-         <div className="con">
-         <BsImage className="icon"/>
-          <p className="drag">Drap your image here to add to your folder</p>
-          <p className="or">or choose your file</p>
-         </div>
-         <input type="file" value='' onChange={onFileDrop}/>
-
+        <UploadArea onFileChange={(files) => onFileChange(files)}>
+          <div className="con">
+            <BsImage className="icon" />
+            <p className="drag">Drap your image here to add to your folder</p>
+            <p className="or">or choose your file</p>
+          </div>
+          <input type="file" value="" onChange={onFileDrop} />
         </UploadArea>
       </FileUploadCon>
     </Container>
--- src/styles/Media.style.js
+++ src/styles/Media.style.js
@@ -3,104 +3,156 @@ import styled from "styled-components";
 export const Container = styled.div`
   padding: 30px;
 `;
-export const UploadMCon = styled.div`
-
-`;
+export const UploadMCon = styled.div``;
 export const UploadText = styled.div``;
 export const FileUploadCon = styled.div`
-margin-top:50px ;
-display:flex ;
-gap:20px ;
+  margin-top: 50px;
+  display: flex;
+  gap: 20px;
 `;
 export const Folder = styled.div`
-flex:1 ;
-background: #FFFFFF;
-box-shadow: 0px 5px 10px #F1F2FA;
-border-radius: 8px;
-height: 400px;
-display:flex ;
-flex-direction:column ;
-
+  flex: 1;
+  background: #ffffff;
+  box-shadow: 0px 5px 10px #f1f2fa;
+  border-radius: 8px;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
 
   .menu-header {
     display: flex;
     align-items: center;
     padding: 10px;
-    justify-content:space-between ;
+    justify-content: space-between;
 
     .menu-title {
       font-style: normal;
       font-weight: 800;
       font-size: 20px;
       line-height: 25px;
-       color: #1c1f37;
+      color: #1c1f37;
     }
-    .create{
-      display:flex ;
-      align-items:center ;
+    .create {
+      display: flex;
+      align-items: center;
     }
+  }
+  .folder1 {
+    padding:10px  15px;
+
+    border-bottom: 1px #dadada solid;
+
+    .identify {
+      display: flex;
+      align-items: center;
+      .fold-icon {
+        color: #017efa;
+        font-size: 25px;
+        margin-right: 10px;
+      }
+      p {
+        font-style: normal;
+        font-weight: 300;
+        font-size: 18px;
+        line-height: 21px;
+        /* identical to box height */
+
+        color: #000000;
+      }
+    }
+    .drop-file {
+      display: flex;
+      flex-direction: column;
+
+    
+      .file-item {
+        display: flex;
+        align-items: center;
+        padding: 10px;
+        margin-left: 25px;
+        .med-icon {
+          margin-right: 5px;
+          color: #51cbff;
+        }
+        .item-name {
+          font-style: normal;
+          font-weight: 300;
+          font-size: 15px;
+          line-height: 17px;
+
+          color: #000000;
+        }
+      }
+    }
+    .add {
+        font-style: normal;
+        font-weight: 800;
+        font-size: 12px;
+        line-height: 15px;
+        display:flex ;
+        align-items:center ;
+        justify-content:flex-end ;
+        color: #017efa;
+      }
+
   }
 `;
 export const UploadArea = styled.div`
-position: relative;
-flex:2 ;
-background:#FFFFFF ;
-border-radius: 8px;
-border: 1px solid #000000;
-box-sizing: border-box;
-box-shadow: 0px 5px 10px #F1F2FA;
-display:flex ;
-flex-direction:column ;
-justify-content:center ;
-align-items:center ;
+  position: relative;
+  flex: 2;
+  background: #ffffff;
+  border-radius: 8px;
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  box-shadow: 0px 5px 10px #f1f2fa;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
-.con{
-  display:flex ;
-flex-direction:column ;
-justify-content:center ;
-align-items:center ;
-}
+  .con {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 
-.drag{
-  font-style: normal;
-font-weight: 300;
-font-size: 25px;
-line-height: 29px;
-/* identical to box height */
+  .drag {
+    font-style: normal;
+    font-weight: 300;
+    font-size: 25px;
+    line-height: 29px;
+    /* identical to box height */
 
-text-align: center;
+    text-align: center;
 
-color: #000000;
-}
+    color: #000000;
+  }
 
-.or{
-  font-weight: 300;
-font-size: 20px;
-line-height: 23px;
-/* identical to box height */
+  .or {
+    font-weight: 300;
+    font-size: 20px;
+    line-height: 23px;
+    /* identical to box height */
 
+    color: #000000;
+  }
 
-color: #000000;
-}
+  .icon {
+    font-size: 100px;
+  }
 
-.icon{
-  font-size:100px ;
-}
+  input {
+    opacity: 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    cursor: pointer;
+  }
 
-input{
-  opacity:0 ;
-  position: absolute;
-  top:0 ;
-  left:0 ;
-  height:100% ;
-  width:100% ;
-  cursor: pointer;
-
- 
-}
-
-
-&:hover{
-    opacity:0.6 ;
+  &:hover {
+    opacity: 0.6;
   }
 `;
